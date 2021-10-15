3f068805ca8a35ffef596de289ac7da35b030f03
LudwinLara21 <87671764+LudwinLara21@users.noreply.github.com>
2021-10-15 09:50:53 -0600

    actu

---
+++ SQLQuery4.sql
@@ -0,0 +1,28 @@
+CREATE TABLE AGENDA
+(
+	nombre varchar (50),
+	apellido varchar (50),
+	domicilio varchar (50),
+	telefono int primary key NOT NULL,
+
+);
+
+insert into AGENDA values ('ludwin', 'zelaya', 'bo. el calvario', 1);
+
+select apellido from AGENDA;
+select nombre from AGENDA;
+select domicilio from AGENDA;
+select telefono from AGENDA;
+
+insert into AGENDA values ('Migue', 'Alas', 'col. veracruz', 78986755);
+
+select nombre from AGENDA;
+select apellido from AGENDA;
+select domicilio from  AGENDA;
+select telefono from AGENDA;
+
+
+
+
+
+
